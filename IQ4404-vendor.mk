459ea2f0dfc7ea291e52d1b6e24801360afee44f
RomanDesigner <romashkarus08@gmail.com>
2019-03-09 23:40:39 +0300

    first and finish commit

---
+++ IQ4404-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Fly/IQ4404/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Fly/IQ4404/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Fly/IQ4404/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Fly/IQ4404/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Fly/IQ4404/proprietary/xbin,system/xbin)
+
